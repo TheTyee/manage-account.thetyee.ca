e335f483682fbb058e07d89b8468229a71274091
Phillip Smith <ps@phillipadsmith.com>
2020-03-15 19:18:38 -0700

    Quick & dirty prototype (new route, templates, etc.)

--- app.py
+++ app.py
@@ -13,15 +13,21 @@ def hello_world():
 @app.route('/account/')
 def account_get():
     """Gets a Recurly account by account_id"""
-    account_id = ''
-    account_id = session['account_id']
+    if 'account_id' in session:
+        account_id = session.get('account_id')
+    else:
+        account_id = '0'
     try:
         account = client.get_account(account_id)
-        return "Got Account %s" % account
+        #return "Got Account %s" % account
+        print(account)
+        return render_template('account_update.html', account=account)
     except recurly.errors.NotFoundError:
+        error = ''
         # If the resource was not found, you may want to alert the user or
         # just return nil
-        return 'Resource Not Found %s' % account_id
+        #return 'Resource Not Found %s' % account_id
+        return render_template('error.html', error=error) 
 
 @app.route('/accounts')
 def accounts_search():
@@ -45,7 +51,20 @@ def accounts_search():
         # Render an failed lookup template with some direction on what to do next
         return json.dumps(account_list)
 
-# TODO Add the route for updating the billing info
-# @app.route('/account_update_billing')
-# """Updates account's billing info"""
+@app.route('/account_update_billing', methods=["POST"])
+def account_update_billing():
+    """Updates account's billing info"""
+    account_id = session.get('account_id')
+    token = request.form['recurly-token']
+    try:
+        billing_update = {"token_id": token}
+        billing = client.update_billing_info(account_id, billing_update)
+        print("Updated BillingInfo %s" % billing)
+        return "Updated billing info"
+    except recurly.errors.ValidationError as e:
+        # If the request was invalid, you may want to tell your user
+        # why. You can find the invalid params and reasons in e.error.params
+        print("ValidationError: %s" % e.error.message)
+        print(e.error.params)
+
 
